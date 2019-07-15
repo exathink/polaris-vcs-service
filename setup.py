22b04b29506e41c692daf982d876a7e05153dfc5
Krishna Kumar <kkumar@exathink.com>
2019-07-15 15:48:28 -0500

    Make buildable package [story=#167264024 subject=setup_vcs_integration_service story_url=https://www.pivotaltracker.com/story/show/167264024]

--- setup.py
+++ setup.py
@@ -11,36 +11,33 @@
 from setuptools import setup
 from os import path
 
-# Suppose the package you are build is called polaris.x.y.z
 
-# First create a package directory called polaris/x/y/z with a valid
-# __init__.py in it. This is a Python 3 Namespace Package.
 
-# UNCOMMENT 'import' line and import the name of the package you are defining.
-
-# import polaris.x.y.z
+import polaris.vcs.service
 
 here = path.abspath(path.dirname(__file__))
 
 
 setup(
     # --------------------------------------------------------------------------------
-    # UNCOMMENT THE 'name' line and replace it with your package name.
 
-    # name='polaris.x.y.z',
+
+    name='polaris-vcs-service',
 
     # -------------------------------------------------------------------------------
-    # UNCOMMENT 'version' and replace the version with the one from the right package.
-    # Your packages __init__.py must have the __version__ property
 
-    # version=polaris.x.y.z.__version__,
+
+    version=polaris.vcs.service.__version__,
 
     # -------------------------------------------------------------------------------
     # UNCOMMENT THE 'packages' line and define the Python 3 namespace packages for this package.
     # This should specify a package for each prefix of your package name.
 
-    # packages=['polaris', 'polaris.x', 'polaris.x.y', 'polaris.x.y.z'],
-
+    packages=[
+        'polaris',
+        'polaris.vcs',
+        'polaris.vcs.service'
+    ],
     url='',
     license = 'Commercial',
     author='Krishna Kumar',
