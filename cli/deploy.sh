184b99fca5d949f040699ee6e61cf9d9ad92a23f
Krishna Kumar <kkumar@exathink.com>
2019-07-21 13:40:03 -0500

    add deploy.sh script [story=#167264024 subject=setup-vcs-service story_url=https://www.pivotaltracker.com/story/show/167264024]

---
+++ cli/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+export DONT_PROMPT_FOR_CONFIRMATION=1
+echo "Deploying Image"
+package aws deploy-image
+
+echo "Deploying Task Definitions.."
+package aws deploy-task-definition polaris-vcs-db-migrator
+package aws deploy-task-definition polaris-vcs-service
+package aws deploy-task-definition polaris-vcs-listener
+
+echo "Running migrations"
+package aws run-task polaris-vcs-db-migrator
+
+echo "Deploying Services.."
+package aws deploy-services polaris-vcs-service polaris.auto-scaling-group polaris-vcs-service polaris-vcs-listener
