50dc06de48431a91a08d2686c3f6d632fbd5f0d7
krishnaku <kkumar@exathink.com>
2019-07-25 09:38:22 -0500

    Add refresh connector repositories message [story=#167432335 subject=fetch-repositories-back-end story_url=https://www.pivotaltracker.com/story/show/167432335]

---
+++ polaris/vcs/messaging/messages/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: © Exathink, LLC (2011-2019) All Rights Reserved
+
+# Unauthorized use or copying of this file and its contents, via any medium
+# is strictly prohibited. The work product in this file is proprietary and
+# confidential.
+
+# Author: Krishna Kumar
+
+from polaris.messaging.messages import register_messages
+
+from .refresh_connector_repositories import RefreshConnectorRepositories
+
+# Add this to the global message factory so that the messages can be desrialized on reciept.
+register_messages([
+    RefreshConnectorRepositories
+])
+
---
+++ polaris/vcs/messaging/messages/refresh_connector_repositories.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: © Exathink, LLC (2011-2018) All Rights Reserved
+
+# Unauthorized use or copying of this file and its contents, via any medium
+# is strictly prohibited. The work product in this file is proprietary and
+# confidential.
+
+# Author: Krishna Kumar
+
+
+
+from marshmallow import fields
+
+from polaris.messaging.messages import Message
+
+
+class RefreshConnectorRepositories(Message):
+    message_type = 'work_items.refresh_connector_repositories'
+
+    connector_key = fields.String(required=True)
+    tracking_receipt_key = fields.String(required=False, allow_none=True)
