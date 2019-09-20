e9aa8eb7fcddde9eced2002cd27a78b98a7a71e5
Krishna Kumar <kkumar@exathink.com>
2019-09-20 15:10:50 -0500

    Implement Fetch Repositories for Gitlab and get_secure_url protocol implementation for https cloning.
    [
    story=#168668650 subject=gitlab-fetch-projects story_url=https://www.pivotaltracker.com/story/show/168668650
    
    ]

--- polaris/vcs/integrations/gitlab.py
+++ polaris/vcs/integrations/gitlab.py
@@ -22,29 +22,45 @@ class GitlabRepositoriesConnector(GitlabConnector):
     def __init__(self, connector):
         super().__init__(connector)
 
-
-
     def map_repository_info(self, repo):
         return dict(
-            name=repo.name,
-            url=repo.html_url,
-            public=not repo.private,
+            name=repo['name'],
+            url=repo['http_url_to_repo'],
+            public=repo['visibility'] == 'public',
             vendor='git',
-            integration_type=VcsIntegrationTypes.github.value,
-            description=repo.description,
-            source_id=repo.id,
+            integration_type=VcsIntegrationTypes.gitlab.value,
+            description=repo['description'],
+            source_id=repo['id'],
             properties=dict(
-                ssh_url=repo.ssh_url,
-                homepage=repo.homepage,
-                default_branch=repo.default_branch,
+                ssh_url=repo['ssh_url_to_repo'],
+                homepage=repo['web_url'],
+                default_branch=repo['default_branch'],
+                path_with_namespace=repo['path_with_namespace']
             ),
         )
 
     def fetch_repositories(self):
-        if self.personal_access_token is not None:
-            pass
-        else:
-            raise ProcessingException("No access token found this Github Connector. Cannot continue.")
+        fetch_repos_url = f'{self.base_url}/projects'
+        while fetch_repos_url is not None:
+            response = requests.get(
+                fetch_repos_url,
+                params=dict(membership=True),
+                headers={"Authorization": f"Bearer {self.personal_access_token}"},
+            )
+            if response.ok:
+                yield response.json()
+                if 'next' in response.links:
+                    fetch_repos_url = response.links['next']['url']
+                else:
+                    fetch_repos_url = None
+            else:
+                raise ProcessingException(
+                    f"Server test failed {response.text} status: {response.status_code}\n"
+                )
 
     def fetch_repositories_from_source(self):
-        pass
+        for repositories in self.fetch_repositories():
+            yield [
+                self.map_repository_info(repo)
+                for repo in repositories
+            ]
