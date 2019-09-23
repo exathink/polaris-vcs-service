8fe02108c1557bbf53ce48331b026ca9f5304658
Krishna Kumar <kkumar@exathink.com>
2019-09-23 10:53:52 -0500

    Call register_webhooks on connector on successfully importing repositories and save the  webhook info on the repository instance. [ story=#168685871 subject=gitlab-push-webhook story_url=https://www.pivotaltracker.com/story/show/168685871
    ]

--- polaris/vcs/commands.py
+++ polaris/vcs/commands.py
@@ -33,11 +33,20 @@ def sync_repositories(connector_key, tracking_receipt_key=None):
                 )
 
 
-def import_repositories(organization_key, repository_keys):
+def register_repository_push_webhooks(organization_key, connector_key, repository_summaries):
+    connector = connector_factory.get_connector(connector_key=connector_key)
+    if connector and getattr(connector, 'register_repository_push_hook', None):
+        for repo in repository_summaries:
+            webhook_info = connector.register_repository_push_hook(repo)
+            api.register_webhook(organization_key, repo['key'], webhook_info)
+
+
+def import_repositories(organization_key, connector_key, repository_keys):
     with db.orm_session():
         result = api.import_repositories(organization_key, repository_keys)
         if result['success']:
             imported_repositories = result['repositories']
+            register_repository_push_webhooks(organization_key, connector_key, imported_repositories)
             publish.repositories_imported(organization_key, imported_repositories)
             return result['repositories']
 
