2714c26f38f04c63d644acd7707e807869c0d136
Krishna Kumar <kkumar@exathink.com>
2021-09-03 06:38:28 -0500

    Implement agent to sync pull_requests_with_analytics
    [PO-847]

--- polaris/vcs/db/api.py
+++ polaris/vcs/db/api.py
@@ -46,14 +46,24 @@ def sync_pull_requests(repository_key, source_pull_requests):
         return db.failure_message('Sync Pull Requests', e)
 
 
-def get_pull_requests_to_sync(before=None, days=3, limit=100):
+def get_pull_requests_to_sync_with_source(before=None, days=3, limit=100):
     try:
         with db.orm_session() as session:
-            return pull_requests.get_pull_requests_to_sync(session, before, days, limit)
+            return pull_requests.get_pull_requests_to_sync_with_source(session, before, days, limit)
     except SQLAlchemyError as exc:
-        return db.process_exception("Get Pull Requests to Sync", exc)
+        return db.process_exception("Get Pull Requests to Sync with Source", exc)
     except Exception as e:
-        return db.failure_message('Get Pull Requests to Sync', e)
+        return db.failure_message('Get Pull Requests to Sync with Source', e)
+
+
+def get_pull_requests_to_sync_with_analytics(before=None, limit=100):
+    try:
+        with db.orm_session() as session:
+            return pull_requests.get_pull_requests_to_sync_with_analytics(session, before=before, limit=limit)
+    except SQLAlchemyError as exc:
+        return db.process_exception("Get Pull Requests to Sync with Analytics", exc)
+    except Exception as e:
+        return db.failure_message('Get Pull Requests to Sync with Analytics', e)
 
 
 def find_pull_request(pull_request_key):
