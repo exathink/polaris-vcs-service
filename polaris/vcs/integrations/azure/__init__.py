62fb3a3ea6ac5b2d190a922c2ebc55e753257d27
Krishna Kumar <kkumar@exathink.com>
2022-04-13 14:33:23 -0500

    [PP-132] Implement Create and Edit Connector UIs for AzureDevOps VCS Connectors.

---
+++ polaris/vcs/integrations/azure/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: © Exathink, LLC (2016-2017) All Rights Reserved
+
+# Unauthorized use or copying of this file and its contents, via any medium
+# is strictly prohibited. The work product in this file is proprietary and
+# confidential.
+
+# Author: Krishna Kumar
+
+from .azure_connector import AzureRepositoriesConnector
+from .azure_connector import AzureRepository
